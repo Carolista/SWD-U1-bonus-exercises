7b4ab9716484388a919c705315b770ddb2320f07
Carolista <cjones.renaissancewoman@gmail.com>
2026-06-18 04:27:00 -0400

    Refactored to inject header and footer content into both HTML files dynamically

--- dom-forms/global.js
+++ dom-forms/global.js
@@ -1,7 +1,19 @@
-// Elements to add shared data
-const year = document.getElementById('year');
+const companyName = 'River City Printing Company';
+const year = new Date().getFullYear();
 
-// Add shared content to <header>
+const header = document.querySelector('header');
+const footer = document.querySelector('footer');
 
-// Add content to <footer>
-year.textContent = new Date().getFullYear();
+if (header) {
+	header.innerHTML = `<h1>&starf; ${companyName} &starf;</h1>`;
+}
+
+if (footer) {
+	footer.innerHTML = `
+		<p>
+			&copy; ${year} ${companyName}, LLC<br />
+			123 Main Street &starf; St. Louis, MO 63101<br />
+			314-555-6789 &starf; info@rivercityprintingstl.com
+		</p>
+	`;
+}
